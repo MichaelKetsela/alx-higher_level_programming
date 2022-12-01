15f55ac7091deba84c4516494beed557a0656a46
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 11:31:29 -0930

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     l = len(sys.argv) -1
     if l == 0:
         print("{} arguments.".format(l))
+    elif l == 1:
+        print("{} argument:".format(l))
     else:
         print("{} arguments:".format(l))
     for i in range(l):
