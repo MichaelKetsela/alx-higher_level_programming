02c1864f89d158026749295b29b90b7c0b76bda1
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 11:18:02 -0930

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import sys
-l = len(sys.argv) -1
-if l == 0:
-    print("{} arguments.".format(l))
-else:
-    print("{} arguments:".format(l))
+
+if __name__ == "__main__":
+
+    import sys
+    l = len(sys.argv) -1
+    if l == 0:
+        print("{} arguments.".format(l))
+    else:
+        print("{} arguments:".format(l))
     for i in range(l):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
