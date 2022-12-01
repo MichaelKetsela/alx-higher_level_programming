acedf3ae2a196b0e4f2051d1d36d87d750a80588
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 12:12:29 -0930

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     operator = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    if sys.argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
