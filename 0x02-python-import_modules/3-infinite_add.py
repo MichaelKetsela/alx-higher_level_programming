54383b636c25ac5f60301959eee00adddbaaec3f
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 11:22:06 -0930

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-sum = 0
-for i in range(len(sys.argv) - 1):
-    sum += int((sys.argv[i + 1])
-print("{}".format(sum))
+if __name__ == "__main__":
+
+    import sys
+
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int((sys.argv[i + 1])
+    print("{}".format(sum))
