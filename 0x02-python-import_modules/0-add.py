65a42a6ce08b71fc3ad93c240e76881dbe7354b8
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 09:58:13 -0930

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-from add_0.py import add
-add(1,2)
+from add_0 import add
+
+if __name__ =="__main__":
+  a, b = 1, 2
+  print("{} + {} = {}".format(a, b, add(a, b)))
+  
+ 
+
+
