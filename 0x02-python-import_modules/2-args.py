13b50088aaa2b2e7afa194fa30f3243e6a7551f5
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-01 09:49:24 -0930

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
-i = 1
-while argv:
-    print("{}: argv[i]".format(i))
-    i++
+l = len(sys.argv)
+if l == 0:
+    print("{} arguments.".format(l))
+else:
+    print("{} arguments:".format(l))
+    for i in range(l):
+        print("{}: {}".format(i, sys.argv[i + 1])
