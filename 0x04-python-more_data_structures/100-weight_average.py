99a51d6506566667c7382e43ee69c03464c9067b
MichaelKetsela <michaelketsela837@gmail.com>
2022-12-08 00:19:13 +0300

    update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-# 100-weight_average.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def weight_average(my_list=[]):
     """Return the weighted average of all integers in a list of tuples."""
-    if not isinstance(my_list, list) or len(my_list) == 0:
+    if not my_list:
         return (0)
 
     avg = 0
     size = 0
-    for tup in my_list:
-        avg += (tup[0] * tup[1])
-        size += tup[1]
+    for i in my_list:
+        avg += (i[0] * i[1])
+        size += i[1]
     return (avg / size)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-# 6-print_sorted_dictionary.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def print_sorted_dictionary(a_dictionary):
-    """Print a dictionary by ordered keys."""
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+    [print("{}: {}".format(x, a_dictionary[x])) for x in sorted(a_dictionary)]
