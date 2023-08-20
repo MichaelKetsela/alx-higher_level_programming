4c706dfe6ecea943ffd2cbce7d1ffe512b858ca6
MichaelKetsela <michaelketsela837@gmail.com>
2023-08-20 22:10:15 +0300

    ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                   (argv[4],))
+              (argv[4],))
     row = c.fetchall()
     for i in row:
         print(i)
